8a3a3924216f294d2d29affb4793bb40ed7228dd
Ayush <ayushpal6939@gmail.com>
2023-02-16 14:07:05 +0530

    new dom file

---
+++ Sharpener/dom.js
@@ -0,0 +1,13 @@
+//Examine the DOCUMENT OBJECT
+
+var headerTitle = document.getElementById('header-title')
+var header = document.getElementById('main-header')
+var Item = document.getElementById('main')
+var Item2 = document.getElementById('Item')
+
+Item2.style.color= 'green'
+Item2.style.fontWeight= '900'
+Item2.style.borderBottom = 'solid 2px #000'
+Item.style.borderBottom= 'solid 3px #000'
+
+header.style.borderBottom= 'solid 3px #000';
