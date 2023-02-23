12fd5df58968ff77a69dcf1247374e05aefc3fb7
Ayush <ayushpal6939@gmail.com>
2023-02-23 16:34:22 +0530

    task12 array of userList

--- Sharpener/dom4.js
+++ Sharpener/dom4.js
@@ -1,7 +1,35 @@
 //to save the form information in local storage
 
 const submitBtn = document.getElementById('getcall');
+const form = document.getElementById('addForm');
+const userList = document.getElementById('items');
 
+//function to add user info in the display list
+form.addEventListener('submit',function(e)
+{
+e.preventDefault()
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+    var phone = document.getElementById('phone').value
+    var date = document.getElementById('date').value
+    var time = document.getElementById('time').value
+
+
+    var li  = document.createElement('li')
+
+    li.classname = 'list-group-item'
+
+    li.appendChild(document.createTextNode(name))
+    li.appendChild(document.createTextNode(email))
+    li.appendChild(document.createTextNode(phone))
+    li.appendChild(document.createTextNode(date))
+    li.appendChild(document.createTextNode(time))
+
+    userList.appendChild(li)
+})
+
+
+//to add user in an array
 submitBtn.addEventListener('click', function() {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
@@ -16,16 +44,22 @@ submitBtn.addEventListener('click', function() {
     date: date,
     time: time
   };
+  let users = [];
 
+  if (localStorage.getItem('users')) {
+    users = JSON.parse(localStorage.getItem('users'));
+  }
 
-  localStorage.setItem('formData', JSON.stringify(formData));
-  JSON.parse(localStorage.getItem('formData'))
+  users.push(formData);
+
+  localStorage.setItem('users', JSON.stringify(users));
+
+//   localStorage.setItem('formData', JSON.stringify(formData));
+  JSON.parse(localStorage.getItem('users'))
   //JSON.stringify() method is used to convert 
   // the JavaScript object to a JSON string, 
   // which is the format required for local storage.
 
 });
 
-console.log(JSON.parse(localStorage.getItem('formData')))
-//JSON.parse method is used to convert the string format of 
-// data stored into object form. 
+console.log(JSON.parse(localStorage.getItem('users')))
