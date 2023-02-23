8f96611d92f17eb2a6184703fe69a146aedefb56
Ayush <ayushpal6939@gmail.com>
2023-02-23 11:35:43 +0530

    task10 bookAppointmentApp

---
+++ Sharpener/dom4.js
@@ -0,0 +1,25 @@
+//to save the form information in local storage
+
+const submitBtn = document.getElementById('getcall');
+
+submitBtn.addEventListener('click', function() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+
+
+  const formData = {
+    name: name,
+    email: email,
+    phone: phone,
+    date: date,
+    time: time
+  };
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+//JSON.stringify() method is used to convert 
+// the JavaScript object to a JSON string, 
+// which is the format required for local storage.
