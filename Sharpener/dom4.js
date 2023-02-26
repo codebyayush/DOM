fb8356c88eeb4ca8f7354fac2451c5cf291282bb
Ayush <ayushpal6939@gmail.com>
2023-02-26 22:56:35 +0530

    task 13 delete user

--- Sharpener/dom4.js
+++ Sharpener/dom4.js
@@ -1,48 +1,26 @@
 //to save the form information in local storage
 
-const submitBtn = document.getElementById('getcall');
+const submitBtn = document.getElementById('submit');
 const form = document.getElementById('addForm');
 const userList = document.getElementById('items');
+// const local = localStorage.getItem('users')
+
+
 
 //function to add user info in the display list
-form.addEventListener('submit',function(e)
-{
-e.preventDefault()
-    var name = document.getElementById('name').value
-    var email = document.getElementById('email').value
-    var phone = document.getElementById('phone').value
-    var date = document.getElementById('date').value
-    var time = document.getElementById('time').value
 
 
-    var li  = document.createElement('li')
-
-    li.classname = 'list-group-item'
-
-    li.appendChild(document.createTextNode(name))
-    li.appendChild(document.createTextNode(email))
-    li.appendChild(document.createTextNode(phone))
-    li.appendChild(document.createTextNode(date))
-    li.appendChild(document.createTextNode(time))
-
-    userList.appendChild(li)
-})
-
-
-//to add user in an array
+//to add users in an array (local storage)
 submitBtn.addEventListener('click', function() {
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const phone = document.getElementById('phone').value;
-  const date = document.getElementById('date').value;
-  const time = document.getElementById('time').value;
-
+  
   const formData = {
     name: name,
     email: email,
-    phone: phone,
-    date: date,
-    time: time
+    phone: phone
+    
   };
   let users = [];
 
@@ -52,14 +30,44 @@ submitBtn.addEventListener('click', function() {
 
   users.push(formData);
 
-  localStorage.setItem('users', JSON.stringify(users));
 
-//   localStorage.setItem('formData', JSON.stringify(formData));
-  JSON.parse(localStorage.getItem('users'))
+//this will show user details on display
+  const userList = document.getElementById('items')
+
+  const nam = document.createElement('li')
+  nam.textContent = formData.name + ' - ' + formData.email + ' - ' + formData.phone
+
+
+//
+
+
+//this will add del functionality clicking del btn
+  const delBtn = document.createElement('input')
+  delBtn.type = 'button'
+  delBtn.value = 'Delete'
+  delBtn.onclick = () => {
+    
+    // let arr = JSON.parse(localStorage.getItem('users'))
+    // let index = arr.indexOf(formData.name)
+    // if (index>-1){
+    //   arr.splice(index, 1);
+    // }
+
+
+    localStorage.removeItem(formData.email)
+    userList.removeChild(nam)
+  }
+  nam.appendChild(delBtn)
+  userList.appendChild(nam)
+  
+//
+
+  localStorage.setItem(formData.email, formData.name);
+
+  // JSON.parse(localStorage.getItem('users'))
   //JSON.stringify() method is used to convert 
   // the JavaScript object to a JSON string, 
-  // which is the format required for local storage.
+  // which is the format required for local storage. 
+  })
 
-});
-
-console.log(JSON.parse(localStorage.getItem('users')))
+// console.log(JSON.parse(localStorage.getItem('users')))
